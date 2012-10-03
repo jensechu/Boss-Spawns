6bc6b99944ef17a4874f37ed7ffd9271809ce777
Jensen <jensechu@gmail.com>
2012-10-02 18:19:29 -0700

    Toned down the max_length for the choices, and added a docstirng

--- bossspawns/deathclock/models.py
+++ bossspawns/deathclock/models.py
@@ -2,22 +2,24 @@ from django.db import models
 
 
 class Boss(models.Model):
-    name = models.CharField(max_length=200)
-    respawn_rate = TimeField('respawn time')
+    """ Object for an individual boss."""
     ZONES = (
         ('SF', 'Sparkfly Fen'),
         ('BS', 'Blazeridge Steppes'),
         ('FS', 'Frostgorge Sound')
     )
-    boss_location = models.CharField(max_length=200, choices=ZONES)
+    name = models.CharField(max_length=200)
+    respawn_rate = TimeField('respawn time')
+    boss_location = models.CharField(max_length=5, choices=ZONES)
 
 class DeathCount(models.Model):
-    boss = models.ForeignKey(Boss)
-    death_time = models.TimeField('time of death')
+    """ """
     SERVERS = (
         ('JQ', 'Jade Quarry'),
         ('DH', 'Dark Haven')
     )
-    server = models.CharField(max_length=200, choices=SERVERS)
+    boss = models.ForeignKey(Boss)
+    death_time = models.TimeField('time of death')
+    server = models.CharField(max_length=5, choices=SERVERS)
     
 
