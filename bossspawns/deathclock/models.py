e823b4807f436a9969bd600639e36d3fe5f72f31
Jensen <jensechu@gmail.com>
2012-10-02 18:05:03 -0700

    Added models for the boss' and the deathcounter

--- bossspawns/deathclock/models.py
+++ bossspawns/deathclock/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Boss(models.Model):
+    name = models.CharField(max_length=200)
+    respawn_rate = TimeField('respawn time')
+    ZONES = (
+        ('SF', 'Sparkfly Fen'),
+        ('BS', 'Blazeridge Steppes'),
+        ('FS', 'Frostgorge Sound')
+    )
+    boss_location = models.CharField(max_length=200, choices=ZONES)
+
+class DeathCount(models.Model):
+    boss = models.ForeignKey(Boss)
+    death_time = models.TimeField('time of death')
+    SERVERS = (
+        ('JQ', 'Jade Quarry'),
+        ('DH', 'Dark Haven')
+    )
+    server = models.CharField(max_length=200, choices=SERVERS)
+    
+
