85cd25ecaccb08517583fb2c2151702fccdd1244
Jensen <jensechu@gmail.com>
2012-10-09 16:19:49 -0700

    Users and death times now in the admin interface for Death Counts

--- bossspawns/deathclock/admin.py
+++ bossspawns/deathclock/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from bossspawns.deathclock.models import Server, Zone, Boss, DeathCount
 
 class DeathCountAdmin(admin.ModelAdmin):
-    list_display = ('boss', 'server')
+    list_display = ('boss', 'server', 'user', 'died_at')
 
 for model in (Server, Zone, Boss):
     admin.site.register(model)
