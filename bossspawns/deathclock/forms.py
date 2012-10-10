b8900dff4950cead5e9a768f67f713e27e120960
Yaroslav Shirokov <slava@github.com>
2012-10-09 20:46:42 -0700

    Kill default, it was always wrong.

--- bossspawns/deathclock/forms.py
+++ bossspawns/deathclock/forms.py
@@ -5,16 +5,16 @@ from pytz import timezone
 from bossspawns.deathclock.models import DeathCount
 
 class DeathCountForm(forms.Form):
-    death_time = forms.DateTimeField(label='Time of death', initial=datetime.now)
+    death_time = forms.DateTimeField(label='Time of death', initial="Click to enter a time")
 
     def save(self, user, boss, server):
         if not self.is_valid():
             return None
         death_time = self.cleaned_data['death_time']
         death_count = DeathCount.objects.create(
-            user=user, 
-            boss=boss, 
-            server=server, 
+            user=user,
+            boss=boss,
+            server=server,
             died_at=death_time
         )
         return death_count
