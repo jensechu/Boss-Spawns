4d3fbf084da3ca8837bcf63947b50dea6823c61d
Jensen <jensechu@gmail.com>
2012-10-15 21:37:33 -0700

    Template for profiles

--- bossspawns/profiles/views.py
+++ bossspawns/profiles/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 
+@login_required
 def my_profile(request):
-    return HttpResponse("yes")
+    
+    return render(request, 'profiles/profile.html', {})
