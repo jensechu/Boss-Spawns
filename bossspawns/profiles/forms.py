1c52ac5b6d97663aba2ee2b00b59c86f99e1b1c6
Jensen <jensechu@gmail.com>
2012-10-16 14:33:26 -0700

    Added a form to set the timezone of a user

---
+++ bossspawns/profiles/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from bossspawns.profiles.models import UserProfile
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
--- bossspawns/profiles/views.py
+++ bossspawns/profiles/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from bossspawns.profiles.forms import ProfileForm
 
 @login_required
 def my_profile(request):
-    
-    return render(request, 'profiles/profile.html', {})
+    profile = request.user.get_profile()
+    profile_form = ProfileForm(instance=profile)
+    return render(request, 'profiles/profile.html', {
+            'profile_form': profile_form
+    })
